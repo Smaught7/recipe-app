666cf5baefc1b4e8e3c6771ae3409748ee29dee7
MJama <mateusz.jama@gmail.com>
2024-06-22 13:03:52 +0200

    setup components

--- training-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ training-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.scss'
+  styleUrl: './recipe-list.component.scss',
 })
 export class RecipeListComponent {
+  public recipes: Recipe[] = [
+    {
+      name: 'Test recipe',
+      description: 'This is test recipe description',
+      imagePath:
+        'https://www.allrecipes.com/thmb/mvO1mRRH1zTz1SvbwBCTz78CRJI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/67700_RichPastaforthePoorKitchen_ddmfs_4x3_2284-220302ec8328442096df370dede357d7.jpg',
+    },
+  ];
 
+  constructor() {}
 }
